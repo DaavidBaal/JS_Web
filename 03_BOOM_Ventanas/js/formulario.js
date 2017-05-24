cffdd1300011721125811ad986d7243e8da6bae3
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-24 17:30:51 +0200

    ejercicio 4

---
+++ 03_BOOM_Ventanas/js/formulario.js
@@ -0,0 +1,15 @@
+
+function main() {
+    document.getElementById("btn_1").onclick = abrirVentana;
+    document.getElementById("btn_2").onclick = cerrarVentana;
+    document.getElementById("btn_3").onclick = cargarImagen;
+    document.getElementById("btn_4").onclick = abrirVentana;
+    document.getElementById("btn_5").onclick = abrirVentana;
+    
+
+}
+
+let oVentana = window.open ("http://www.elmundo.es/");
+
+
+ oVentana.;
---
+++ 04_tiempo/js/main.js
@@ -0,0 +1,22 @@
+var nombre
+
+function muestraMensaje () {
+    alert ('Han trascurrido 3 segundos desde que me programaron');
+}
+
+function pideDatos (pFuncion){
+    let nombre
+    nombre = prompt ("Dime tu nombre");
+    pFuncion(nombre);
+ 
+}
+
+function escribe(pNombre) {
+ console.log("El nombre es: " + pNombre);
+}
+
+let id = window.setTimeout (function(){
+    pideDatos(escribe);
+  
+}, 3000)
+
