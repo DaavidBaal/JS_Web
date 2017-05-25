4e131721315bee7911c66a7ae6a91900937a159c
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-25 16:48:30 +0200

    ejercicio 9

---
+++ 09_DOM_JQ/js/main.js
@@ -0,0 +1,19 @@
+//$("document").ready
+
+
+$(function main  () {
+	//Voy a manipular el DOM
+	 //let js = document.querySelector("#primera")
+	//let jq = $("#primera")
+
+	let js = document.querySelectorAll("section")
+	let jq =$("section").
+	
+
+	console.dir(js)
+	console.dir(jq)
+	
+	
+	$("section").append() = "<p>Hola</p>"
+})
+
