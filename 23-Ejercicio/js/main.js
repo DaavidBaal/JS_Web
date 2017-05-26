61ef293cd621add2d66d052e9cad041f219ca957
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-26 19:24:59 +0200

    ejercicio validación

--- 23-Ejercicio/js/main.js
+++ 23-Ejercicio/js/main.js
@@ -2,14 +2,8 @@
 
 //Creamoas el acceso a los formularios
 
-document.forms[0];
-
-function recoger() {
-	let correo = document.getElementById("email").value;
-	let contraseña = document.getElementById("password").value;
-	document.write(nombre)
-	document.write(password)
-
-}
-
-
+class Formulario{
+		constructor (){
+			
+		}
+};
